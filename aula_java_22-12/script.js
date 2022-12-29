69a7ac92547c8ef1d3d3323eb55533ebbf642bc4
Renata <renatas.pa@hotmail.com>
2022-12-28 21:55:12 -0300

    finalizando exercicio do dia 22/12

--- aula_java_22-12/script.js
+++ aula_java_22-12/script.js
@@ -3,6 +3,7 @@ const error_text = document.getElementsByClassName('error-text')
 const username_input = document.getElementById('login-usuario')
 const password_input = document.getElementById('login-senha')
 const success_message = document.getElementsByClassName('success-message')
+const fail_message = document.getElementsByClassName('fail-message')
 
 //console.log(username_input.value)
 //console.log(password_input.value)
@@ -14,8 +15,12 @@ const success_message = document.getElementsByClassName('success-message')
 
 function validateUsernameInput() {
     if (username_input.value != 'renata') {
+        username_input.classList.remove('correct')
+        username_input.classList.add('error')
         error_text[0].classList.add('visible')
     } else {
+        username_input.classList.add('correct')
+        username_input.classList.remove('error')
         error_text[0].classList.remove('visible')
     }
 }
@@ -23,12 +28,23 @@ function validateUsernameInput() {
 
 function validatePasswordInput() {
     if (password_input.value != '123') {
+        password_input.classList.remove('correct')
+        password_input.classList.add('error')
         error_text[1].classList.add('visible')
     } else {
+        password_input.classList.add('correct')
+        password_input.classList.remove('error')
         error_text[1].classList.remove('visible')
     }
 }
 
-function showSuccessMessage() {
-    success_message[0].classList.toggle('flex')
+function showMessage() {
+    if (username_input.value != 'renata' || password_input.value != '123') {
+        success_message[0].classList.remove('visible')
+        fail_message[0].classList.add('visible')
+    
+    } else {
+        fail_message[0].classList.remove('visible')
+        success_message[0].classList.add('visible')
+    }
 }
