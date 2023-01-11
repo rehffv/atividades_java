39df46746bd8e4a4272ef59c51d7bc344258c697
Renata <renatas.pa@hotmail.com>
2023-01-10 22:33:38 -0300

    fim da aula

--- aula_java_10-01-23/script.js
+++ aula_java_10-01-23/script.js
@@ -1,6 +1,26 @@
 const linkPerfil = document.getElementById("link-perfil");
 const navPerfil = document.getElementById("nav-perfil");
+const linkDados = document.getElementById("link-perfil-dados");
+const eventoVoltar = document.getElementById("evento-voltar");
 
-linkPerfil.addEventListener("mouseover", ()=> {
-  navPerfil.style.display = "block"
+//linkPerfil.addEventListener("mouseover", () => {
+//  navPerfil.style.display = "block"
+//})
+
+window.addEventListener('keyup', (event) => {
+  //console.log(linkDados)
+
+  if (event.key === '1') {
+    if(navPerfil.style.display == "block") {
+      linkDados.click()        
+
+    }else{
+      navPerfil.style.display = "block"
+    }
+  
+  }else if (event.code === 'Escape' || event.code === 'Backspace'){
+    navPerfil.style.display = "none"
+    eventoVoltar.click()
+  }
+
 })
